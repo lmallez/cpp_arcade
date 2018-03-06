12bd612c69827a2f7a0ad4c6502ca0a4c30a596b
Louis Mallez <louis.mallez@epitech.eu>
2018-03-06 16:11:33 +0100

    First Commit // CMake + Architecture

---
+++ src/core/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_arcade
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	std::cout << "SALUT A TOUS LES GIGAS BROS" << std::endl;
+}

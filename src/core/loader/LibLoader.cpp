05f86fd965b5c9d7c14e9cf4fd75215c14183efa
faudil.puttilli <faudil.puttilli@epitech.eu>
2018-03-20 16:31:05 +0100

    libLoader created

--- src/core/loader/LibLoader.cpp
+++ src/core/loader/LibLoader.cpp
@@ -6,3 +6,44 @@
 //
 
 #include "LibLoader.hpp"
+
+LibLoader::LibLoader(const std::string &libName) :
+	_libName(libName), _sym(nullptr)
+{
+	load(libName);
+}
+
+bool LibLoader::operator!() const
+{
+	return _sym == nullptr;
+}
+
+bool LibLoader::load(const std::string &libName)
+{
+	_libName = libName;
+
+	_sym = dlopen(libName.c_str(), RTLD_LAZY);
+	std::string err_msg = dlerror();
+	if (err_msg.empty())
+		return true;
+	std::cerr << err_msg << std::endl;
+	return false;
+}
+
+bool LibLoader::unload()
+{
+	if (_sym == nullptr)
+		return false;
+	dlclose(_sym);
+	_sym = nullptr;
+	_libName = "";
+	return true;
+}
+
+const std::unique_ptr<arc::IGraphic> LibLoader::getIGraphic()
+{
+	std::unique_ptr<arc::IGraphic> _lol;
+
+
+	return std::unique_ptr<arc::IGraphic>();
+}
--- src/core/loader/LibLoader.hpp
+++ src/core/loader/LibLoader.hpp
@@ -8,11 +8,21 @@
 #ifndef CPP_ARCADE_LIBLOADER_HPP
 #define CPP_ARCADE_LIBLOADER_HPP
 
+#include <dlfcn.h>
+#include <iostream>
+#include <src/graphic/IGraphic.hpp>
 
 class LibLoader
 {
 public:
-	
+	explicit LibLoader(const std::string & = "");
+	bool operator!() const;
+	bool load(const std::string &);
+	bool unload();
+	const std::unique_ptr<arc::IGraphic> getIGraphic();
+private:
+	std::string _libName;
+	void *_sym;
 };
 
 

45d4d5abb9dced5a5ec60b4ba56bf6eeb0d55407
faudil.puttilli <faudil.puttilli@epitech.eu>
2018-04-06 16:50:40 +0200

    Preparlib now compile SDL lib

--- prepareLib.sh
+++ prepareLib.sh
@@ -3,10 +3,12 @@
 cd cmake-build-debug
 make GRAPHIC_Caca -j4
 make GRAPHIC_SFML -j4
+make GRAPHIC_SDL -j4
 make GAME_BasicGame -j4
 make GAME_SNAKE -j4
 cd ..
 cp cmake-build-debug/libGRAPHIC_SFML.so ./graphics
+cp cmake-build-debug/libGRAPHIC_SDL.so ./graphics
 cp cmake-build-debug/libGRAPHIC_Caca.so ./graphics
 cp cmake-build-debug/libGAME_BasicGame.so ./games
 cp cmake-build-debug/libGAME_SNAKE.so ./games

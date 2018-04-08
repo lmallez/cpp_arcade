c7ac0695ce5520b7ac2a272adcd90bdeb30a3b85
faudil.puttilli <faudil.puttilli@epitech.eu>
2018-04-08 16:36:31 +0200

    ShapeText now has a background with SDL2

--- src/graphic/SDL/draw/SDLShapeText.cpp
+++ src/graphic/SDL/draw/SDLShapeText.cpp
@@ -23,6 +23,9 @@ void arc::SDLShapeText::draw() const
 {
 	SDL_Rect geo = winGeometry();
 	gfxPrimitivesSetFont(nullptr, 42, 42);
+	boxColor(arc::SDLMainWindow::getInstance().getRenderer().get(),
+		 geo.x, geo.y, geo.w + geo.x, geo.h + geo.y, _texture.bgColor()
+			 .values());
 	stringColor(arc::SDLMainWindow::getInstance().getRenderer().get(),
 		    (Sint16) geo.x, (Sint16) geo.y,
 		getText().c_str(), _texture.lineColor().values());
